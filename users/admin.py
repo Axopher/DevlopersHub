8aa3c0270179b077edcb9a73d713531f5383a57c
Axopher <69104459+Axopher@users.noreply.github.com>
2023-03-01 14:05:58 +0545

    devhub project

---
+++ users/admin.py
@@ -0,0 +1,8 @@
+import profile
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Skill)
+admin.site.register(Message)
